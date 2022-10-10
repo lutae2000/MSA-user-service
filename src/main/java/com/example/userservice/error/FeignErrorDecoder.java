12e7f99f91239727aa93495e80373f6dcf2bb991
utlee <lutae2000@gmail.com>
2022-10-11 01:02:24 +0900

    Feign 구현1

---
+++ src/main/java/com/example/userservice/error/FeignErrorDecoder.java
@@ -0,0 +1,33 @@
+package com.example.userservice.error;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+@Component
+public class FeignErrorDecoder implements ErrorDecoder {
+    Environment environment;
+    @Autowired
+    public FeignErrorDecoder(Environment environment){
+        this.environment = environment;
+    }
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        switch (response.status()){
+            case 400:
+                break;
+            case 404:
+                if(methodKey.contains("getOrders"))
+                    return new ResponseStatusException(HttpStatus.valueOf(response.status()),
+                            environment.getProperty("order_service.orders_is_empty"));
+                break;
+            default:
+                return new Exception(response.reason());
+
+        }
+        return null;
+    }
+}
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,18 +1,16 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.UserDto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -32,12 +30,15 @@ public class UserServiceImpl implements UserService {
     BCryptPasswordEncoder passwordEncoder;
     Environment environment;
     RestTemplate restTemplate;
+    OrderServiceClient orderServiceClient;
+
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, Environment environment, RestTemplate restTemplate) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, Environment environment, RestTemplate restTemplate, OrderServiceClient orderServiceClient) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.environment = environment;
         this.restTemplate = restTemplate;
+        this.orderServiceClient = orderServiceClient;
     }
 
     @Override
@@ -78,14 +79,25 @@ public class UserServiceImpl implements UserService {
 //        List<ResponseOrder> orders = new ArrayList<>();
         //String orderURL = "http://localhost:8000/order-service/%s/orders";
         //String.format 사용하면 파라미터에 넣을수 있음
-        String orderURL = String.format(environment.getProperty("order_service.url"), userId);
+/*        String orderURL = String.format(environment.getProperty("order_service.url"), userId);
         ResponseEntity<List<ResponseOrder>> orderListResponse = restTemplate.exchange(orderURL,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ResponseOrder>>() {
-        });
+        });*/
 
-        List<ResponseOrder> orderList = orderListResponse.getBody();
+        //List<ResponseOrder> orderList = orderListResponse.getBody();
+/*        List<ResponseOrder> orderList = null;
+        try {
+            orderList = orderServiceClient.getOrders(userId);
+        } catch (FeignException ex){
+            log.error("ex.getMessage(): " + ex.getMessage());
+            log.error("============================================================================");
+            log.error("ex.getCause(): " + ex.getCause());
+        }*/
+
+        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+
 
         userDto.setOrders(orderList);
         return userDto;
